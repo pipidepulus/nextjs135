29292b7a5db6c567e19f557b330206c84824ff8d
pipidepulus <pipidepulus@hotmail.com>
2023-12-07 13:51:12 -0500

    delay 3rd commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+// import { UserButton } from "@clerk/nextjs";
+// import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+      {/* <Button className="primary-gradient min-h[46px] px-4 py-3 !text-light-900">
+        Ask a Question
+      </Button> */}
     </div>
   );
 }
